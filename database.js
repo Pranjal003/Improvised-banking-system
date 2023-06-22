38fbbea17873c55964d12220a5f3e8ac0a27bb83
Pranjal Sharma <92199952+Pranjal003@users.noreply.github.com>
2023-06-22 09:39:14 +0530

    Update database.js

--- database.js
+++ database.js
@@ -7,7 +7,7 @@ mongoose.connect("mongodb://localhost:27017/newdb01" ,
 {
  
     useNewUrlParser:true,
-    useUnifiedTopology:true,
+    useUnifiedTopology:true, 
     useCreateIndex:true,
     strictQuery: true
 
